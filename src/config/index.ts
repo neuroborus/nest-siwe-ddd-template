68a08a0dc600a18e4b1c1932969281d4b4e84db5
Michael <mihasso@protonmail.com>
2026-02-23 21:13:08 +0100

    chore(config): stop tracking test env file and align migration/test config   exports

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,4 @@
 export { ConfigModule } from './config.module';
-export { staticConfig } from './static-config';
 export { NodeEnv } from './node-env.enum';
-export { requireEnv } from './envs/require-env';
 export { databaseConfig } from './database.config';
+export { staticConfig } from './static-config';
